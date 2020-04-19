c0c6cd5997d3c9e7b809b76356293b04f5be379e
Marcelo Correa <marcelocorrea.0827@outlook.com>
2020-04-19 20:48:02 -0300

    Classes do Dominio Invoice, Vehicle e CarRental, foram criadas

---
+++ Entities/Invoice.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace SolSemInterface.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+
+        // Propriedade Calculada
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return "Basic payment"
+                + BasicPayment.ToString("F2", CultureInfo.InvariantCulture)
+                + "\Tax: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + "\Total payment: "
+                + TotalPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ namespace SolSemInterface
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
